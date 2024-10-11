89e531693eb7ddee3a789ddedeef48ca9891b544
Lumy0726 <jinny9807@gmail.com>
2024-10-11 17:40:14 +0900

    Generated MAVLink C header, commit 237b97f2bae5605e22057667fd2b9f3ad748f4f6

--- development/development.h
+++ development/development.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_DEVELOPMENT.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_DEVELOPMENT_XML_HASH 7223560110779520647
+#define MAVLINK_DEVELOPMENT_XML_HASH -2420621665386018359
 
 #ifdef __cplusplus
 extern "C" {
