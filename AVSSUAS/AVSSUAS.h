a7d84e367bef96fbcf98a019c801d58a5d3cf6af
Lumy0726 <jinny9807@gmail.com>
2024-10-14 21:09:12 +0900

    Generated MAVLink C header, commit 4f8b81fb1631e93362751663cc91248944dcc42a

--- AVSSUAS/AVSSUAS.h
+++ AVSSUAS/AVSSUAS.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_AVSSUAS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_AVSSUAS_XML_HASH 1253302038865680779
+#define MAVLINK_AVSSUAS_XML_HASH 1270966803828007641
 
 #ifdef __cplusplus
 extern "C" {
--- uAvionix/uAvionix.h
+++ uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH -2307624787792924483
+#define MAVLINK_UAVIONIX_XML_HASH -6962198565180970171
 
 #ifdef __cplusplus
 extern "C" {
