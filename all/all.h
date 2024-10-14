4c6b9a38246a08750a3b905658d6d71a5099025e
Lumy0726 <jinny9807@gmail.com>
2024-10-14 13:43:52 +0900

    Generated MAVLink C header, commit d46f9bcff0ac93c387e8ccee4f13277f3020ab7d

--- all/all.h
+++ all/all.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ALL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ALL_XML_HASH 2713964070505492020
+#define MAVLINK_ALL_XML_HASH -5705075932023712040
 
 #ifdef __cplusplus
 extern "C" {
