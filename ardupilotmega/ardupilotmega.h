9e3ba34d43aec7f57268cf1a2389038429a9b6a0
Lumy0726 <jinny9807@gmail.com>
2024-10-17 17:30:18 +0900

    Generated MAVLink C header, commit 1720994ccdd37bf11ed29543149157e2f10bb97a

--- ardupilotmega/ardupilotmega.h
+++ ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH 8143173785196494183
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH -614732535694309959
 
 #ifdef __cplusplus
 extern "C" {
