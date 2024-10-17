f1408ee4193aaa078e681bb3b4cbdd63dfeff83d
Lumy0726 <jinny9807@gmail.com>
2024-10-17 17:55:29 +0900

    Generated MAVLink C header, commit 30040359bc554c3b5c329801f32bd6ce55827341

--- icarous/icarous.h
+++ icarous/icarous.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ICAROUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ICAROUS_XML_HASH -2215039370652542886
+#define MAVLINK_ICAROUS_XML_HASH -105693303982491865
 
 #ifdef __cplusplus
 extern "C" {
