3b40dcfc7878a8fffe2d88f2f683635311cebf2e
Lumy0726 <jinny9807@gmail.com>
2024-10-19 06:52:23 +0900

    Generated MAVLink C header, commit 7ed658ef058f91baa29b0bfb7ba20cee9f03e4aa

--- standard/standard.h
+++ standard/standard.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_STANDARD.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_STANDARD_XML_HASH 6031491667412021076
+#define MAVLINK_STANDARD_XML_HASH 4609822676681418121
 
 #ifdef __cplusplus
 extern "C" {
--- ualberta/ualberta.h
+++ ualberta/ualberta.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UALBERTA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UALBERTA_XML_HASH 6207534852503210974
+#define MAVLINK_UALBERTA_XML_HASH 2254472714396161376
 
 #ifdef __cplusplus
 extern "C" {
